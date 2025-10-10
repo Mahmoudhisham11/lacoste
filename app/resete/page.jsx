a94e6f61e95948344e3f3cd421591f0fd3fe1432
Mahmoud Hisham <mahmoudhisham11@gmail.com>
2025-10-11 02:54:05 +0300

    Add new files and updates

--- app/resete/page.jsx
+++ app/resete/page.jsx
@@ -86,7 +86,7 @@ function Resete() {
       <div className={styles.text}>
         <p>المدفوع: {invoice.total}$</p>
         <p>المتبقي: 0.0</p>
-        <p>عدد الاصناف:<span style={{border: '2px solid black', padding: "5px"}}>{invoice.lenth}</span></p>
+        <p>عدد الاصناف:<span style={{border: '2px solid black', padding: "5px"}}>{invoice.length}</span></p>
         <p>العنوان: الخصوص شارع القسم بجوار محل دلوعة</p>
         <p style={{ textAlign: 'center', marginTop: '5px'}}>رقم الهاتف: 01113306404</p>
         <p style={{ textAlign: 'center', marginTop: '5px'}}>رقم الكاش: 01095164379</p>
